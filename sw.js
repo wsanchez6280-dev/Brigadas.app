35554dc2136c1201960e2fec28ef2cbf8a6046fc
wsanchez6280-dev <wsanchez6280@gmail.com>
2026-09-09 20:31:54 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'brigada-app-v19-herramientas-admin';
+const ARCHIVOS = [
+  './', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo-hospital.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore-compat.js',
+  'https://www.gstatic.com/firebasejs/10.14.1/firebase-auth-compat.js'
+];
+
+self.addEventListener('install', (evento) => {
+  evento.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ARCHIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evento) => {
+  evento.waitUntil(
+    caches.keys().then((nombres) =>
+      Promise.all(nombres.filter((n) => n !== CACHE).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evento) => {
+  evento.respondWith(
+    caches.match(evento.request).then((respuestaCache) => {
+      return respuestaCache || fetch(evento.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
